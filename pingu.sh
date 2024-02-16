d5a9fd0859d0b390937ce0cb2609f939ab19a6b0
mateus-msb <mateus.silvaborges99@gmail.com>
2024-02-16 17:39:52 -0300

    Clean output from terminal

--- pingu.sh
+++ pingu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the venv from the repository and execute the app
-"$HOME/Codes/PY/pingu/env/bin/python" "$HOME/Codes/PY/pingu/app.py" &
+"$HOME/Codes/PY/pingu/env/bin/python" "$HOME/Codes/PY/pingu/app.py" > /dev/null 2>&1 &
 
 # Open webpage in firefox
 firefox localhost:5000 &
