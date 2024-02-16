54dc532c95078a33d351b64dcd01f03df9559531
mateus-msb <mateus.silvaborges99@gmail.com>
2024-02-16 17:48:17 -0300

    Clean output from opening firefox

--- pingu.sh
+++ pingu.sh
@@ -4,4 +4,4 @@
 "$HOME/Codes/PY/pingu/env/bin/python" "$HOME/Codes/PY/pingu/app.py" > /dev/null 2>&1 &
 
 # Open webpage in firefox
-firefox localhost:5000 &
+firefox localhost:5000 > /dev/null 2>&1 &
